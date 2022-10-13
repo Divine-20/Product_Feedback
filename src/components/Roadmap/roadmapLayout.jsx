37b8cda52754f8cdce679941b3ae18bed9f53205
Divine-20 <divineingabire69@gmail.com>
2022-10-13 22:17:06 +0200

    removed comments

--- src/components/Roadmap/roadmapLayout.jsx
+++ src/components/Roadmap/roadmapLayout.jsx
@@ -3,8 +3,6 @@ import { FaAccusoft } from 'react-icons/fa'
 import SuggestionContent from '../suggestions/suggestionContent'
 import Sidebar from '../suggestions/sidebar'
 // import Roadmapdetails from './roadmapdetails'
-
- 
 function RoadmapLayout() {
   return (
     <div>
